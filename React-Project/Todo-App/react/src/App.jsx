5e088f653710a02fb0b7a19d19255be29a141eee
samKit88 <samuel.girma.cx9896@gmail.com>
2022-06-09 16:12:09 +0300

    header

--- React-Project/Todo-App/react/src/App.jsx
+++ React-Project/Todo-App/react/src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
+//components
+import Todo from './components/Todo'
+
 const App = () => {
-  return <div>Todo</div>
+  return (
+    <div>
+      <Todo />
+    </div>
+  )
 }
 
 export default App
