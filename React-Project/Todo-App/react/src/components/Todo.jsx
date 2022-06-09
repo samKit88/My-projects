e0ac6ee81d854611e21471f99b6fd2d25b3fbd5a
samKit88 <samuel.girma.cx9896@gmail.com>
2022-06-09 16:47:16 +0300

    list-items

--- React-Project/Todo-App/react/src/components/Todo.jsx
+++ React-Project/Todo-App/react/src/components/Todo.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+
+let count = 0
 
 const Todo = () => {
+  const [todos, setTodos] = useState([])
+  const todoItem = useRef('')
+
+  const handelBTN = () => {
+    setTodos([
+      ...todos,
+      { num: count++, text: todoItem.current.value, discription: '' },
+    ])
+    todoItem.current.value = ''
+    console.log(todoItem.current.value)
+  }
+
   return (
     <div className="todo">
       <div className="todo-header">
@@ -9,11 +23,21 @@ const Todo = () => {
             type="text"
             placeholder="Add your plan"
             className="todo-input"
+            ref={todoItem}
           />
           <div className="todo-add-btn">
-            <button>Add</button>
+            <button onClick={handelBTN}>Add</button>
           </div>
         </div>
+      </div>
+      <div className="todo-list">
+        <ul>
+          {todos.map((item) => (
+            <li key={item.num}>
+              {item.text} <button>delete</button> <button>edit</button>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )
